bc08bf76d45e8c97afa6b4d54b21cc1a150df18d
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-03-26 23:01:32 -0400

    Added MM.py and palindrome.py in dynamic programming

---
+++ 4_dynammic_programming/Matrix_chain_multiplication.py
@@ -0,0 +1,36 @@
+import sys
+
+def MatrixChainOrder(p, n):
+    # For simplicity of the program, one extra row and one
+    # extra column are allocated in m[][].  0th row and 0th
+    # column of m[][] are not used
+    m = [[0 for x in range(n)] for x in range(n)]
+ 
+    # m[i, j] = Minimum number of scalar multiplications needed
+    # to compute the matrix A[i]A[i + 1]...A[j] = A[i..j] where
+    # dimension of A[i] is p[i-1] x p[i]
+ 
+    # cost is zero when multiplying one matrix.
+    for i in range(1, n):
+        m[i][i] = 0
+ 
+    # L is chain length.
+    for L in range(2, n):
+        for i in range(1, n-L + 1):
+            j = i + L-1
+            m[i][j] = sys.maxsize
+            for k in range(i, j):
+ 
+                # q = cost / scalar multiplications
+                q = m[i][k] + m[k + 1][j] + p[i-1]*p[k]*p[j]
+                if q < m[i][j]:
+                    m[i][j] = q
+ 
+    return m[1][n-1]
+
+arr = [10, 5, 20, 50, 40]
+size = len(arr)
+ 
+print("Minimum number of multiplications is " +
+       str(MatrixChainOrder(arr, size)))
+# This Code is contributed by Bhavya Jain
