801804724c27551af5ccbe86ed099ce7eb60d4e8
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-07-05 12:06:04 -0400

    Daily update

--- 3_trees/preorder.py
+++ 3_trees/preorder.py
@@ -1,4 +1,5 @@
-# Definition for a binary tree node.
+from typing import Optional, List
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -19,3 +20,7 @@ class Solution:
         dfs(root)
         return preorder
 
+def cmp(a: int, b: int) -> int:
+    return (a > b) - (a < b)
+
+print(cmp(5,4))
