c66d154339abeae88bdd3935b35cc8d342473c8d
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-07-12 17:11:56 -0400

    Daily update

---
+++ 3_trees/314_binary_tree_vertical_order_traversal.py
@@ -0,0 +1,78 @@
+"""Medium, Meta"""
+from tree import TreeNode, Tree
+from collections import defaultdict, deque
+from typing import List, Optional
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def verticalOrder_dfs(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+
+        columnTable = defaultdict(list)
+        min_column = max_column = 0
+
+        def DFS(node, row, column):
+            if node is not None:
+                nonlocal min_column, max_column
+                columnTable[column].append((row, node.val))
+                min_column = min(min_column, column)
+                max_column = max(max_column, column)
+
+                # preorder DFS
+                DFS(node.left, row + 1, column - 1)
+                DFS(node.right, row + 1, column + 1)
+
+        DFS(root, 0, 0)
+
+        # order by column and sort by row
+        ret = []
+        for col in range(min_column, max_column + 1):
+            columnTable[col].sort(key=lambda x:x[0])
+            colVals = [val for row, val in columnTable[col]]
+            ret.append(colVals)
+
+        return ret
+    
+    def verticalOrder_bfs_sorting(self, root: TreeNode) -> List[List[int]]:
+        columnTable = defaultdict(list)
+        queue = deque([(root, 0)])
+
+        while queue:
+            node, column = queue.popleft()
+
+            if node is not None:
+                columnTable[column].append(node.val)
+                
+                queue.append((node.left, column - 1))
+                queue.append((node.right, column + 1))
+                        
+        return [columnTable[x] for x in sorted(columnTable.keys())]
+        
+
+    def verticalOrder_bfs_non_sorting(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+
+        columnTable = defaultdict(list)
+        min_column = max_column = 0
+        queue = deque([(root, 0)])
+
+        while queue:
+            node, column = queue.popleft()
+
+            if node is not None:
+                columnTable[column].append(node.val)
+                min_column = min(min_column, column)
+                max_column = max(max_column, column)
+
+                queue.append((node.left, column - 1))
+                queue.append((node.right, column + 1))
+
+        return [columnTable[x] for x in range(min_column, max_column + 1)]
--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Leetcode Grind
 
+
 ## Tree
 - [606. Construct String from Binary Tree](3_trees/606_construct_string_from_binary_tree.py)
 - [1650. LCA of a Binary Tree](3_trees/1650_lca.py)
+- [314. Binary Tree Vertical Order Traversal](3_trees/314_binary_tree_vertical_order_traversal.py)
+[Tree doc in details](3_trees/Tree.md)
+
+## Todo
+- [ ] DFS inorder, postorder Traversal
+- [ ] BFS inorder, postorder Traversal
+- [ ] Figure Lambda function in [314. Binary Tree Vertical Order Traversal](3_trees/314_binary_tree_vertical_order_traversal.py)
+
+```python
+# A lambda function that adds 10 to its argument
+add_ten = lambda x: x + 10
+print(add_ten(5))  # Output: 15
+```
