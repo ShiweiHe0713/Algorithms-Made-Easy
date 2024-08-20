b36c00073b0bbafc30b62b1a0131643da0ef8b1c
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-08-20 14:55:37 -0400

    Daily update

--- 9_graphs/1254_number_of_closed_islands.py
+++ 9_graphs/1254_number_of_closed_islands.py
@@ -1,7 +1,7 @@
 from typing import List
-
+from collections import deque
 class Solution:
-    def closedIsland(self, grid: List[List[int]]) -> int:
+    def closedIsland_dfs(self, grid: List[List[int]]) -> int:
         # if the island connected to the boarder cell, mark the closed as False
         def dfs(i: int, j: int) -> bool:
             # reaching the boundary, threatening the flag
@@ -26,6 +26,40 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 0 and dfs(i, j):
+                    count += 1
+        
+        return count
+    
+    def closedIsland_bfs(self, grid: List[List[int]]) -> int:
+        # if the island connected to the boarder cell, mark the closed as False
+        def bfs(i: int, j: int) -> bool:
+            queue = deque()
+            queue.append((i, j))
+            closed = True
+
+            while queue:
+                # get the new indices
+                i, j = queue.popleft()
+                for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    nx, ny = i + x, j + y
+
+                    if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                        closed = False
+                        continue
+                    if grid[nx][ny] == 1:
+                        continue
+                    
+                    queue.append((nx, ny))
+                    grid[nx][ny] = 1
+                
+            return closed
+
+        # increment count iff grid is land and dfs(grid) is True
+        m, n = len(grid), len(grid[0])
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0 and bfs(i, j):
                     count += 1
         
         return count
