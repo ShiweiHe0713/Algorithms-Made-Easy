0a58c66bb93b467eb7d3fdb20ca59cb8546b44d0
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-08-26 13:59:40 -0400

    Daily update

--- 10_dp/divide_coins.py
+++ 10_dp/divide_coins.py
@@ -1,8 +1,8 @@
 from typing import List
 
 def divide_coins(coins: List[int], amount: int) -> List[int]:
-    if amount in memo:
-        return memo[amount]
+    if amount in coins:
+        return 1
     
     result = [None] * len(coins)
 
@@ -19,18 +19,20 @@ def divide_coins(coins: List[int], amount: int) -> List[int]:
                 break
             if rem < coins[j]:
                 continue
-        
+            
             while rem - coins[j] >= 0:
                 rem -= coins[j]
                 count += 1
-            
-        if rem == 0:
-            result[i] = count
-        else:
-            result[i] = -1
 
+            result[i] = count if rem == 0 else -1
+    
+    print(result)
     return result[-1]
 
 coins = [1,3,5,8,17,21]
+# result= [1,3,3,2,3,3]
 memo = {}
-print(divide_coins(coins, 21))
+divide_coins(coins, 21)
+
+# 17, 8
+# 17 = 8 + 8 + 1
