049d3d6362a412d26c6a52233ba90e826efd69e4
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-08-27 20:49:30 -0400

    Daily update

---
+++ 9_graphs/2812_find_the_safest_path_in_a_grid.py
@@ -0,0 +1,42 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
+        # The safeness factor of a path is defined as the minimum MD from any cell in the path to any thief in the grid. 路径中，小偷到最近的cell的曼哈顿距离叫Path's Safeness Factor 
+        # 要求返回所有到最右下格子的路径的最大safeness factor, 即找到里小偷最远的路径
+
+        # gather all thieves for bfs
+        thieves = deque()
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    thieves.append((i, j))
+        
+        level = 0
+        visited = set()
+        
+        while thieves:
+            i, j = thieves.popleft()
+            flag = True
+            visited.add((i, j))
+
+            for x, y in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                nx, ny = i + x, j + y
+                # stop if reached start or end cell or visted
+                if not (0 <= nx < m and 0 <= ny < n):
+                    continue
+                if (nx, ny) not in visited and (nx, ny) not in [(0,0), (m-1,n-1)]:
+                    thieves.append((nx, ny))
+                else:
+                    flag = False
+            
+            if flag:
+                level += 1
+
+        return level
+
+s = Solution()
+grid = [[1,0,0],[0,0,0],[0,0,1]]
+print(s.maximumSafenessFactor(grid))
