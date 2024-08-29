31958458e9512ca0929ee0555ca970be8de19f66
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-08-29 11:59:58 -0400

    Daily update

--- 2_two_pointers/443_two_pointers.py
+++ 2_two_pointers/443_two_pointers.py
@@ -2,25 +2,7 @@ from typing import List
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        """Modify i directly in the inner loop"""
-        i = 0
-        while i < len(chars):
-            j = i
-            while i + 1 < len(chars) and chars[i+1] == chars[i]:
-                i += 1
-            i += 1
-            count = i - j
-
-            if count != 1:
-                # we can replace a subarray with an array!???
-                chars[j+1:i] = list(str(count))
-                # decremen i to next differnt char
-                i -= (i-j-1) - len(str(count))
-
-        return len(chars)
-    
-    def compress_II(self, chars: List[str]) -> int:
-        """Create tmp var j in the inner loop"""
+        """Recommended: Create tmp var j in the inner loop"""
         i = 0
         while i < len(chars):
             j = i
@@ -37,3 +19,23 @@ class Solution:
 
 
         return len(chars)
+    
+    def compress_II(self, chars: List[str]) -> int:
+        """Less recommended: Modify i directly in the inner loop"""
+        i = 0
+        while i < len(chars):
+            j = i
+            while i + 1 < len(chars) and chars[i+1] == chars[i]:
+                i += 1
+            i += 1
+            count = i - j
+
+            if count != 1:
+                # we can replace a subarray with an array!???
+                chars[j+1:i] = list(str(count))
+                # decremen i to next differnt char
+                i -= (i-j-1) - len(str(count))
+
+        return len(chars)
+    
+    
