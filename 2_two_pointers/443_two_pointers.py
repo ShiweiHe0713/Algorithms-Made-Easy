c0cc43629a0dc789bfd96491d1d871995fe36317
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-08-29 11:58:27 -0400

    Daily update

--- 2_two_pointers/443_two_pointers.py
+++ 2_two_pointers/443_two_pointers.py
@@ -2,6 +2,7 @@ from typing import List
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
+        """Modify i directly in the inner loop"""
         i = 0
         while i < len(chars):
             j = i
@@ -15,5 +16,24 @@ class Solution:
                 chars[j+1:i] = list(str(count))
                 # decremen i to next differnt char
                 i -= (i-j-1) - len(str(count))
+
+        return len(chars)
+    
+    def compress_II(self, chars: List[str]) -> int:
+        """Create tmp var j in the inner loop"""
+        i = 0
+        while i < len(chars):
+            j = i
+            while j + 1 < len(chars) and chars[j+1] == chars[j]:
+                j += 1
+            j += 1
+            count = j - i
+
+            if count != 1:
+                chars[i+1:j] = list(str(count))
+                i += len(str(count)) + 1
+            else:
+                i += 1
+
 
         return len(chars)
