8ad543af6c109dc1fb1f5c96459e994d3052c854
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-09-06 13:19:57 -0400

    Daily update

---
+++ 2_binary_search/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
+        n = len(nums)
+        l, r = 0, n-1
+        result = []
+        
+        while l < r:
+            mid = l + (r - l) // 2
+            # found left bound, and we only have to find the left bound
+            if nums[mid] == target and (mid == 0 or nums[mid-1] != target):
+                result.append(mid)
+                j = mid
+                while j < n and nums[j] == target:
+                    j += 1
+                result.append(j-1)
+                break
+
+            # move l and r pointer
+            if nums[mid] < target:
+                l = mid + 1
+            # we only have to find left bound, so we can alway shrink to the left
+            else:
+                r = mid
+
+        if not result and nums[l] == target:
+            result += [l, l]
+        
+        return result if result else [-1, -1]
+            
+            
