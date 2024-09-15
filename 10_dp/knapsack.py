68cb3d8e8f653a7c4ebd6c33f6771e06d01866b2
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-09-15 10:32:51 -0400

    Daily update

--- 10_dp/knapsack.py
+++ 10_dp/knapsack.py
@@ -9,11 +9,32 @@ def knapsack_01(capacity: int, weights: List[int], values: List[int]) -> int:
     m, n = len(weights), capacity
     dp = [[0] * (n+1) for _ in range(m+1)]
 
-    for i in range(1, m):
-        for j in range(1, n):
-            if weights[i] > j:
+    for i in range(1, m+1):
+        for j in range(n+1):
+            if weights[i-1] > j:
                 dp[i][j] = dp[i-1][j]
             else:
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weights[i]] + values[i])
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j] - weights[i-1] + values[i-1])
+    
+    return dp[-1][-1]
+
+def knapsack_01_1d(capacity: int, weights: List[int], values: List[int]) -> int:
+    pass
+
+def knapsack_complete(capacity: int, weights: List[int], values: List[int]) -> int:
+    # i is the ID of an element, j is the a current capacity
+    # dp[i][j]: The larget value we get from using the first i elements with a capacity of j
+    # i == len(weights), j == capacity, return dp[len(weights)][capacity] aka dp[-1][-1]
+    # recurrence relationship:
+    # dp[i][j] = max(dp[i-1][j-weights[i]] + values[i], dp[i-1][capacity])
+    m, n = len(weights), capacity
+    dp = [[0] * (n+1) for _ in range(m+1)]
+
+    for i in range(1, m+1):
+        for j in range(n+1):
+            if weights[i-1] > j:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-weights[i-1]] + values[i-1])
     
     return dp[-1][-1]
--- 2_binary_search/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ 2_binary_search/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -30,5 +30,3 @@ class Solution:
             result += [l, l]
         
         return result if result else [-1, -1]
-            
-            
