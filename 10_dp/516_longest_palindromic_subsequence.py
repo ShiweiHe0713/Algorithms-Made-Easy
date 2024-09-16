b8592c1b2968c8cc880d16f66913bfa1f76218d4
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-09-16 17:06:20 -0400

    Daily update

--- 10_dp/516_longest_palindromic_subsequence.py
+++ 10_dp/516_longest_palindromic_subsequence.py
@@ -20,5 +20,15 @@ class Solution:
         return dfs(0, n-1)
     
     def longestPalindromeSubseq_tab(self, s: str) -> int:
-        pass
-        
+        n = len(s)
+        dp = [[0] * (n+1) for _ in range(n+1)]
+
+        for i in range(n, -1, -1):
+            dp[i][i] = 1
+            for j in range(i+1, n+1):
+                if s[i-1] == s[j-1]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+
+        return dp[1][n] 
