951b6409d79bc4d4ca7ce1633d08cb60f0eeef16
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-09-27 10:44:15 -0400

    Daily update

--- 6_trees/110_Balanced_Binary_Tree.py
+++ 6_trees/110_Balanced_Binary_Tree.py
@@ -9,6 +9,7 @@ class TreeNode:
 
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        """T: O(n), S: O(n)"""
         def dfs(node) -> int:
             nonlocal flag
             if not node:
@@ -25,3 +26,41 @@ class Solution:
         flag = True
         dfs(root)
         return flag
+
+    def isBalanced_I(self, root: Optional[TreeNode]) -> bool:
+        """T: O(n^2)(worst case), O(nlogn), S: O(n)"""
+        def height(node: Optional[TreeNode]) -> int:
+            if not node:
+                return -1
+
+            return 1 + max(height(node.left), height(node.right))
+        
+        if not root:
+            return True
+        if abs(height(root.left) - height(root.right)) >= 2:
+            return False
+
+        return self.isBalanced(root.left) and self.isBalanced(root.right) 
+
+    def isBalanced_II(self, root: Optional[TreeNode]) -> bool:
+        """T: O(n), S: O(n)"""
+        def helper(node: Optional[TreeNode]) -> (int, bool):
+            if not node:
+                return (-1, True)
+
+            left_h, left_balanced = helper(node.left)
+            if not left_balanced:
+                return left_h, False
+                
+            right_h, right_balanced = helper(node.right)
+            if not right_balanced:
+                return right_h, False
+
+            height = 1 + max(left_h, right_h)
+            balanced = abs(left_h - right_h) < 2
+            
+            return (height, balanced)
+        
+        return helper(root)[1]
+
+        
